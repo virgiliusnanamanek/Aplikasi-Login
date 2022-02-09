0adaa40a8284ea005db2390e4d00d802a13e2283
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-02-09 10:25:41 +0700

    Update login-script.js
    sedikit simulasi

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,6 +3,7 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ *sedikit
  */
 
 const loginFormElement = document.querySelector('#loginForm');
